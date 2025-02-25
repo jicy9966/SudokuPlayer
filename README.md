63e130a30a5f91f4fc6c3c35ec8682226824bc1c
Brian Ji <jicy@uci.edu>
2025-02-25 04:27:44 -0800

    JAR File executable

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Sudoku Game - Java Swing
 This is a Sudoku game built using Java Swing for the graphical user interface (GUI). 
 The game features a 9x9 Sudoku grid, with pre-filled numbers, and supports Undo/Redo functionality.
 
-Start the game program from the SudokuPlayer.java file.
+Open the command prompt at the directory of this folder.
+Run the following command line to run the game: 
+
+java -jar SudokuPlayer.jar
--- src/SudokuPlayer.java
+++ src/SudokuPlayer.java
@@ -4,10 +4,18 @@ public class SudokuPlayer extends JFrame
 {
     public static void main(String[] args)
     {
-        // begins the game with selecting game difficulty
-        ChooseDifficulty frame = new ChooseDifficulty();
-        frame.setTitle("Choose Game Difficulty");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+
+        SwingUtilities.invokeLater(new Runnable() 
+        {
+            public void run() 
+            {
+                // begins the game with selecting game difficulty
+                ChooseDifficulty frame = new ChooseDifficulty();
+                frame.setTitle("Choose Game Difficulty");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+        
     }
 }
